bc93e8b639dfafbec714171b6e175ac72fe88090
clara-cdp <clarianne.cdp@gmail.com>
2025-11-26 11:30:39 +0100

    created a nivell-1.php file

---
+++ nivell-1.php
@@ -0,0 +1,10 @@
+<?php
+// EXCERCICI 1
+// Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla/terminal.
+
+$int=8;
+$double=4.5;
+$string = "Hello world!";
+$boolean = true; 
+
+?>
