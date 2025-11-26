17697d531fd0974446d5b3a3b5aeb630959cee69
clara-cdp <clarianne.cdp@gmail.com>
2025-11-26 21:30:55 +0100

    starting exercise

--- nivell-1.php
+++ nivell-1.php
@@ -1,10 +1,24 @@
 <?php
-// EXCERCICI 1
-// Defineix una variable de cada tipus: integer, double, string i boolean. Imprimeix-les per pantalla/terminal.
 
-$int=8;
-$double=4.5;
-$string = "Hello world!";
-$boolean = true; 
+/******  EXCERCICI 1  ******/
 
-?>
+$num = 8;
+var_dump($num);
+echo "<br>";
+$numDoble = 4.5;
+var_dump($numDoble);
+echo "<br>";
+$phrase = 'hello world!';
+var_dump($phrase);
+echo "<br>";
+$bools = true;
+var_dump($bools);
+echo "<br>";
+define("NOM", "Clara CDP");
+echo "<h1>"NOM"</h1>";
+
+/******* EXERCICI 2 *******/
+
+echo $phrase;
+
+echo "here we go again!";
