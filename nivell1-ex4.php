65f348d63243a7b6ede2310874fba61b5b0ca686
clara-cdp <clarianne.cdp@gmail.com>
2025-11-30 11:22:26 +0100

    nivell1-ex4 completat

---
+++ nivell1-ex4.php
@@ -0,0 +1,21 @@
+<?php
+/* **** Exercici 4 **** 
+
+Fes un programa que implementi una funció on es compti fins a un nombre determinat. 
+Si no s’inclou un nombre determinat, el nombre haurà de tenir un valor per defecte igual a 10. 
+A més, aquesta funció ha de tenir un segon paràmetre que indiqui de quant a quant es compta(D'1 en 1, de 2 en 2…). 
+El compte s’ha de mostrar per pantalla pas per pas. */
+
+function countingNums($step, $setNum = 10,)
+
+{
+    for ($i = 0; $i <= $setNum; $i += $step) {
+        echo "$i,";
+    }
+    echo "\n";
+}
+
+countingNums(1, 3);
+countingNums(2);
+countingNums(3, 20);
+countingNums(10, 100);
